0015810d95cd2db311c85f994490612f17864845
slim01 <s.slimani@gmx.de>
2015-12-10 12:32:38 +0100

    fill tag devs

--- public/js/paper.js
+++ public/js/paper.js
@@ -3,10 +3,7 @@ $(document).ready(function() {
   var url = window.location.href;
   var id = url.substring(url.lastIndexOf('#') + 1);
 
-  // polyfill from http://stackoverflow.com/questions/1420881/how-to-extract-base-url-from-a-string-in-javascript
-  if (typeof location.origin === 'undefined') location.origin = location.protocol + '//' + location.host;
-
-  $.get(location.origin + '/getPaperById', {'id' : id}, function(data, textStatus, jqXHR) {
+  $.get('http://localhost:8080/getPaperById', {'id' : id}, function(data, textStatus, jqXHR) {
     $('#papertitle').append(data.title);
     var d = new Date(data.publicaton_date);
     //let y = d.getFullYear();
@@ -27,7 +24,48 @@ $(document).ready(function() {
 
       var regexp = /:!:([^\s]+)\s/g;	//regular expression for current tagForm 
       var tempHtml = $('#paperframe').contents().find('html').html();
-	    tempHtml = tempHtml.replace(regexp, "</p><div class='replaceable' id='$1'></div><p class='ltx_p'>"); 		
+      var tempHtml = $('#paperframe').contents().find('html').html();
+      var counter = 0; 
+      tempHtml = tempHtml.replace(regexp, function(match, p1){     
+        counter++; 
+        return "</p><div class='replaceable' style='height: 180px; width: 100px' id='"+ counter + p1 +"'></div><p class='ltx_p'>"; 
+         
+      });     	
+	  
+      $('.replaceable').each(function(index, element) {
+        var elementID = element.getAttribute('id');
+	    	
+        if(/^.*\.[t|T][i|I][f|F]$/.test(elementID)){
+          var map2 = L.map(elementID).setView([51.505, -0.09], 3);     
+          var basemap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
+              maxZoom: 18,
+              attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+              '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+              'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+              id: 'mapbox.streets'
+              }).addTo(map2); 
+			
+     
+        }		
+        else if(/^.*\.[r|R][d|D][a|A][t|T][a|A]$/.test(fileExt)){
+			
+			
+          alert("div found .rdata data"); 
+			
+			
+        }	
+        else if(/^.*\.[j|J][s|S][o|O][n|N]$/.test(fileExt)){
+			
+			
+          alert("div found .json data"); 
+			
+			
+        }			
+      });
+    
+		
+		
+  
 
       //set content of iframe to new html 
       var iframe = document.getElementById('paperframe');
@@ -37,6 +75,8 @@ $(document).ready(function() {
 
     $('#paperframe')[0].src = iframeurl[1];
 
-	}, 'json');
+    }, 'json');
+	
+	
 	
 });
