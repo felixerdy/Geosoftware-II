c9290c97a2a8a2f1fbc2c296862361ac669d40b1
C. Hackenberg <c_hack05@wwu.de>
2015-12-12 14:40:44 +0100

    add geojson, but it sucks yet...

--- public/js/iframe.js
+++ public/js/iframe.js
@@ -1,8 +1,39 @@
 "use strict"
 
+var maps = [];
 
- $(document).ready(function() {
-//function loadPaper() {
+/**
+ * @returns the layer object, optional
+ */
+function createBaseLayer(map) {
+  return L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.streets'
+  }).addTo(map);
+}
+
+function createGJSONLayer(map, url) {
+
+  $.getJSON(url, function(data) {
+    var geojson = L.geoJson(data, {
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup(feature.properties.name);
+      }
+    });
+
+    geojson.addTo(map);
+    map.fitBounds(geojson.getBounds());
+  });
+
+}
+
+$(document).ready(function() {
+
+  var paperid = window.location.pathname;
+  paperid = paperid.replace(/^\/([0-9a-z]+)\/.*$/, "$1");
 
   //replace tags:
   //current tag form "[...] :!:myData.tif [...]"
@@ -13,33 +44,28 @@
   document.body.innerHTML = document.body.innerHTML.replace(regexp, function(match, p1) {
     counter++;
 
-    return "</p><div class='replaceable' style='height: 250px ; width: 60% ' id='" + counter + p1 + "'></div><p class='ltx_p'>";
+    return "</p><div class='replaceable' style='height: 250px ; width: 60% ' id='replaced" + counter + "' dataid='" + p1 + "'></div><p class='ltx_p'>";
   });
 
   $('.replaceable').each(function(index, element) {
     var elementID = element.getAttribute('id');
-    if (/^.*\.[t|T][i|I][f|F]$/.test(elementID)) {
-      var map2 = L.map(elementID).setView([51.505, -0.09], 3);
-      var basemap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        id: 'mapbox.streets'
-      }).addTo(map2);
+    var dataID = element.getAttribute('dataid');
+    if (/^.*\.[t|T][i|I][f|F]$/.test(dataID)) {
+      maps.push( L.map(elementID).setView([51.505, -0.09], 3) );
+      createBaseLayer( maps[maps.length - 1] );
 
-
-    } else if (/^.*\.[r|R][d|D][a|A][t|T][a|A]$/.test(elementID)) {
+    } else if (/^.*\.[r|R][d|D][a|A][t|T][a|A]$/.test(dataID)) {
 
 
       alert("div found .rdata data");
 
 
-    } else if (/^.*\.[j|J][s|S][o|O][n|N]$/.test(elementID)) {
+    } else if (/^.*\.[j|J][s|S][o|O][n|N]$/.test(dataID)) {
 
 
-      alert("div found .json data");
-
+      maps.push( L.map(elementID).setView([51.505, -0.09], 3) );
+      createBaseLayer( maps[maps.length - 1] );
+      createGJSONLayer( maps[maps.length - 1], "../geojson/" +  dataID);
 
     }
   });
