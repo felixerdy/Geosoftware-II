cc49718019b215443a855168ee1458bb0a942525
slim01 <s.slimani@gmx.de>
2016-01-07 13:23:33 +0100

    include leaflet markercluster (1/2)

--- models/latex2html.js
+++ models/latex2html.js
@@ -53,7 +53,10 @@ exports.convert = function(inputdir, input, paper) {
           '<script src="/js/bowerstuff/Flot/jquery.flot.js"></script>' +
           '<script src="/js/bowerstuff/Flot/jquery.flot.navigate.js"></script>' +
           '<script src="/js/bowerstuff/Flot/jquery.flot.resize.js"></script>' +
-          '<script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>');
+          '<script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>' + 
+	        '<script src="https://api.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/leaflet.markercluster.js"></script>' + 
+          '<link href="https://api.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/MarkerCluster.css" rel="stylesheet" />' +
+          '<link href="https://api.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/MarkerCluster.Default.css" rel="stylesheet" />');
 
         fs.writeFileSync(path.join(inputdir, path.basename(input, ".tex") + ".html"), htmlData);
 
